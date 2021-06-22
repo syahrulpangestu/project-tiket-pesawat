d9c999bd4ed62e8fb1e08db2742cd6d49999f44b
syahrulpangestu <syahrulpangestu11@gmail.com>
2021-06-22 20:13:44 +0700

    menambahakan db, crud maskapai, create tiket

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,18 +1,29 @@
 package controllers;
 
+import connectivity.ConnectionClass;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import models.Maskapai;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    @FXML
+    private Label lblStatus1;
 
     @FXML
     private Button createMaskapai;
@@ -30,38 +41,183 @@ public class Controller implements Initializable {
     private Button history;
 
     @FXML
-    private TextField input;
-
-    @FXML
-    private Button createButton;
-
-    @FXML
-    private Pane pnlStatus;
-
-    @FXML
     private Button close;
 
     @FXML
-    private Label lblStatus;
+    private GridPane pnHistory;
 
     @FXML
-    private GridPane pnMaskapai;
+    private TableColumn<?, ?> tableHistoryId;
 
     @FXML
-    private GridPane pnTiket;
+    private TableColumn<?, ?> tableHistoryMaskapai;
+
+    @FXML
+    private TableColumn<?, ?> tableHistoryTujuan;
+
+    @FXML
+    private TableColumn<?, ?> tableHistoryWaktu;
+
+    @FXML
+    private TableColumn<?, ?> tableHistoryJumlah;
+
+    @FXML
+    private TableColumn<?, ?> tableHistoryKelas;
+
+    @FXML
+    private TableColumn<?, ?> tableHistoryTotal;
+
+    @FXML
+    private TextField inputSearchHistory;
+
+    @FXML
+    private Button buttonSearchHistory;
 
     @FXML
     private GridPane pnShowMaskapai;
 
     @FXML
+    private TableColumn<?, ?> tableShowMaskapaiId;
+
+    @FXML
+    private TableColumn<?, ?> tableShowMaskapai;
+
+    @FXML
+    private TableColumn<?, ?> tableShowMaskapaiAksi;
+
+    @FXML
+    private TextField inputSearchMaskapai;
+
+    @FXML
+    private Button buttonShowMaskapai;
+
+    @FXML
     private GridPane pnShowTiket;
 
     @FXML
-    private GridPane pnHistory;
+    private TableColumn<?, ?> tableShowTiketId;
+
+    @FXML
+    private TableColumn<?, ?> tableShowTiketMaskapai;
+
+    @FXML
+    private TableColumn<?, ?> tableShowTiketTujuan;
+
+    @FXML
+    private TableColumn<?, ?> tableShowTiketWaktu;
+
+    @FXML
+    private TableColumn<?, ?> tableShowTiketJumlah;
+
+    @FXML
+    private TableColumn<?, ?> tableShowTiketKelas;
+
+    @FXML
+    private TableColumn<?, ?> tableShowTiketStok;
+
+    @FXML
+    private TableColumn<?, ?> tableShowTiketAksi;
+
+    @FXML
+    private TextField inputSearchShowTiket;
+
+    @FXML
+    private Button buttonSearchShowTiket;
+
+    @FXML
+    private GridPane pnTiket;
+
+    @FXML
+    private ComboBox<String> optionMaskapai;
+
+    @FXML
+    private TextField inputTujuan;
+
+    @FXML
+    private DatePicker inputWaktu;
+
+    @FXML
+    private TextField inputStok;
+
+    @FXML
+    private TextField inputKelas;
+
+    @FXML
+    private Button createButtonTiket;
+
+    @FXML
+    private GridPane pnMaskapai;
+
+    @FXML
+    private TableView<Maskapai> tableViewMaskapai;
+
+    @FXML
+    private TableColumn<Maskapai, Integer> colIdMaskapai;
+
+    @FXML
+    private TableColumn<Maskapai, String> tableMaskapai;
+
+    @FXML
+    private TextField inputCreateMaskapai;
+
+    @FXML
+    private TextField inputUpdateMaskapai;
+
+    @FXML
+    private Button createButtonMaskapai;
+
+    @FXML
+    private Button updateButtonMaskapai;
+
+    @FXML
+    private Button deleteButtonMaskapai;
+
+    @FXML
+    private Pane pnlStatus;
+
+    @FXML
+    private Label lblStatus;
+
+    @FXML
+    void buttonCreateMaskapai(ActionEvent event) {
+        if (event.getSource() == createButtonMaskapai){
+            insertMaskapai();
+        }else if (event.getSource() == updateButtonMaskapai){
+            updateMaskapai();
+        }else if (event.getSource() == deleteButtonMaskapai){
+            deleteMaskapai();
+        }
+    }
+
+    @FXML
+    void buttonCreateTiket(ActionEvent event) {
+        if(event.getSource() == createButtonTiket){
+            insertTiket();
+        }
+
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        showMaskapai();
+        showOptionMaskapai();
+    }
 
+    private void showOptionMaskapai() {
+        ConnectionClass connectionClass = new ConnectionClass();
+        Connection connection =connectionClass.getConnection();
+        Statement st;
+        ObservableList<String> list = FXCollections.observableArrayList();
+        try {
+            ResultSet rs = connection.createStatement().executeQuery("SELECT * FROM maskapai");
+            while (rs.next()){
+                list.add(rs.getString("namaMaskapai"));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        optionMaskapai.setItems(null);
+        optionMaskapai.setItems(list);
     }
 
     @FXML
@@ -94,6 +250,71 @@ public class Controller implements Initializable {
         }
     }
 
+    public ObservableList<Maskapai> getMaskapaiList(){
+        ObservableList<Maskapai> maskapaiList = FXCollections.observableArrayList();
+        ConnectionClass connectionClass = new ConnectionClass();
+        Connection connection =connectionClass.getConnection();
+        String sql = "SELECT * FROM MASKAPAI";
+        Statement st;
+        ResultSet rs;
+        try {
+            st = connection.createStatement();
+            rs = st.executeQuery(sql);
+            Maskapai maskapai;
+            while (rs.next()){
+                maskapai = new Maskapai(rs.getInt("idMaskapai"), rs.getString("namaMaskapai"));
+                maskapaiList.add(maskapai);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return maskapaiList;
+    }
+
+    public void showMaskapai(){
+        ObservableList<Maskapai> list = getMaskapaiList();
+        colIdMaskapai.setCellValueFactory(new PropertyValueFactory<Maskapai, Integer>("idMaskapai"));
+        tableMaskapai.setCellValueFactory(new PropertyValueFactory<Maskapai, String>("namaMaskapai"));
+        tableViewMaskapai.setItems(list);
+    }
+
+    private void insertMaskapai(){
+        String sql = "INSERT INTO maskapai (namaMaskapai) VALUES('"+inputCreateMaskapai.getText()+"')";
+        executeQuery(sql);
+        showMaskapai();
+    }
+
+    private void updateMaskapai(){
+        String sql = "UPDATE maskapai SET namaMaskapai = '"+inputCreateMaskapai.getText()+"' WHERE idMaskapai = "+inputUpdateMaskapai.getText()+"";
+        executeQuery(sql);
+        showMaskapai();
+    }
+
+    private void insertTiket() {
+        String sql = "INSERT INTO tiket (namaMaskapai, tujuan, waktu, stok, kelas) VALUES('"+optionMaskapai.getValue()+"','"+inputTujuan.getText()+"','"+inputWaktu.getValue()+"',"+inputStok.getText()+",'"+inputKelas.getText()+"')";
+        executeQuery(sql);
+        showMaskapai();
+        System.out.println("lalalal"+inputWaktu.toString());
+    }
+
+    private void executeQuery(String sql) {
+        ConnectionClass connectionClass = new ConnectionClass();
+        Connection connection =connectionClass.getConnection();
+        Statement st;
+        try {
+            st = connection.createStatement();
+            st.executeUpdate(sql);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    private void deleteMaskapai(){
+        String sql = "DELETE FROM maskapai WHERE idMaskapai = "+inputUpdateMaskapai.getText()+"";
+        executeQuery(sql);
+        showMaskapai();
+    }
+
     public void hover(MouseEvent mouseEvent) {
         if(mouseEvent.getSource() == createMaskapai){
             createMaskapai.setStyle("-fx-background-color: #076180;-fx-font-size:17;");
@@ -103,8 +324,8 @@ public class Controller implements Initializable {
             showMaskapai.setStyle("-fx-background-color: #076180;-fx-font-size:17;");
         }else if(mouseEvent.getSource() == showTiket){
             showTiket.setStyle("-fx-background-color: #076180;-fx-font-size:17;");
-        }else if(mouseEvent.getSource() == createButton){
-            createButton.setStyle("-fx-background-color: #076180;");
+        }else if(mouseEvent.getSource() == createButtonMaskapai){
+            createButtonMaskapai.setStyle("-fx-background-color: #076180;");
         }else if(mouseEvent.getSource() == close){
             close.setStyle("-fx-background-color: #076180;-fx-font-size:17;");
         }else if(mouseEvent.getSource() == history){
@@ -121,8 +342,8 @@ public class Controller implements Initializable {
             showMaskapai.setStyle("-fx-background-color: transparent;");
         }else if(mouseEvent.getSource() == showTiket){
             showTiket.setStyle("-fx-background-color: transparent;");
-        }else if(mouseEvent.getSource() == createButton){
-            createButton.setStyle("-fx-background-color:  #006680;");
+        }else if(mouseEvent.getSource() == createButtonMaskapai){
+            createButtonMaskapai.setStyle("-fx-background-color:  #006680;");
         }else if(mouseEvent.getSource() == close){
             close.setStyle("-fx-background-color: transparent;");
         }else if(mouseEvent.getSource() == history){
